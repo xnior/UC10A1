5a2f96fcde396612efd55290c0634b4b48b654bc
Adhemar Jr <adhemar.machado@gmail.com>
2022-01-31 19:40:54 -0300

    Adequação de sistema de Segurança Front-End

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -18,14 +18,30 @@ export class LoginComponent implements OnInit {
   mensagem=""
 
   
+  
 
   onLogin(){
+    let erroPalavra:number=0     
+    const blackList:string[] = ["--", ";--", ";", "/*", "*/", "@@", "char", "nchar", "varchar", "nvarchar", "alter", "begin", "cast", "create", "cursor", "declare", "delete","drop", "end", "exec", "execute", "fetch", "insert", "kill", "open", "select", "sys", "sysobjects", "syscolumns", "table", "update"]
     console.log(this.loginModel)
-    this.loginService.login(this.loginModel).subscribe((response)=>{
-      console.log(response)
-    },(respostaErro)=>{
-    this.mensagem=respostaErro.error
-    console.log(this.mensagem)
+    
+    blackList.forEach(palavra=>{
+      // console.log("Palavra no texto: " + palavra)
+      if (this.loginModel.email?.toLowerCase().includes(palavra)) {
+        this.mensagem="Palavra inválida: " + palavra
+        erroPalavra=1
+      }
     })
+    
+    if (erroPalavra == 0 ) {
+
+      this.loginService.login(this.loginModel).subscribe((response)=>{
+        console.log(response)
+      },(respostaErro)=>{
+      this.mensagem=respostaErro.error
+      console.log(this.mensagem)
+      })    
+    }
+    
   }
 }
